bd44d859a51c51e45f4005d63bae1c3212d3063a
Akil0814 <akil040814@gmail.com>
2026-08-08 01:33:17 -0700

    update ppt

--- 0-Introduction/reference/02.cpp
+++ 0-Introduction/reference/02.cpp
@@ -15,11 +15,11 @@ int main()
 
         BeginDrawing();
 
-        ClearBackground(Color{24, 28, 36, 255});
+        ClearBackground(BLACK);
 
-        DrawLine(80, 80, 420, 200, Color{255, 196, 61, 255});
-        DrawCircleV(Vector2{180,120}, 70.0f, Color{72, 187, 120, 255});
-        DrawRectangle(880, 140, 180, 180, Color{99, 179, 237, 255});
+        DrawLine(80, 80, 420, 200, GOLD);
+        DrawCircleV(Vector2{180,120}, 70.0f, GREEN);
+        DrawRectangle(880, 140, 180, 180, SKYBLUE);
 
         EndDrawing();
     }
--- 0-Introduction/reference/03.cpp
+++ 0-Introduction/reference/03.cpp
@@ -20,10 +20,10 @@ int main()
 
         BeginDrawing();
 
-        ClearBackground(Color{24, 28, 36, 255});
-        DrawLine(80, 80, 420, 200, Color{255, 196, 61, 255});
-        DrawCircleV(mouse_position, 70.0f, Color{72, 187, 120, 255});
-        DrawRectangle(880, 140, 180, 180, Color{99, 179, 237, 255});
+        ClearBackground(BLACK);
+        DrawLine(80, 80, 420, 200, GOLD);
+        DrawCircleV(mouse_position, 70.0f, GREEN);
+        DrawRectangle(880, 140, 180, 180, SKYBLUE);
 
         EndDrawing();
     }
